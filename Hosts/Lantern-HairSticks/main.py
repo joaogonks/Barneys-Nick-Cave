71a4f79945aed5642992daff7838c6695db2a48d
João Costa <joaopedrocosta@me.com>
2016-11-09 14:48:47 -0500

    Added host-specific main.py | Adde __init__py | Created Logs folder

---
+++ Hosts/Lantern-HairSticks/main.py
@@ -0,0 +1,40 @@
+import json
+import Queue
+import settings
+import time
+import threading
+import yaml
+
+#from thirtybirds.Logs.main import Exception_Collector
+from thirtybirds.Network.manager import init as network_init
+
+def network_status_handler(msg):
+    print "network_status_handler", msg
+
+def network_message_handler(msg):
+    try:
+        #print "network_message_handler", msg
+        topic = msg[0]
+        host, sensor, data = yaml.safe_load(msg[1])
+        # print "Exception Received:", ex
+    except Exception as e:
+        print "exception in network_message_handler", e
+network = None
+
+def init(HOSTNAME):
+    global network
+    network = network_init(
+        hostname=HOSTNAME,
+        role="client",
+        discovery_multicastGroup=settings.discovery_multicastGroup,
+        discovery_multicastPort=settings.discovery_multicastPort,
+        discovery_responsePort=settings.discovery_responsePort,
+        pubsub_pubPort=settings.pubsub_pubPort,
+        message_callback=network_message_handler,
+        status_callback=network_status_handler
+    )
+
+    network.subscribe_to_topic("system")  # subscribe to all system messages
+    network.subscribe_to_topic("exceptions")
+    network.subscribe_to_topic("Lantern")
+    network.subscribe_to_topic("HairSticks")
